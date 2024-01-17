1c556dcadf58df1d57ddffcdfed35336b8109d57
Hamzajan04 <156242412+Hamzajan04@users.noreply.github.com>
2024-01-17 21:43:11 +0500

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,35 @@
+#hamza Ali jan
+
+
+#Write a Python program to do arithmetical operations addition and division
+#addition
+
+#give variable the numbers
+num1=(5)
+num2=(6)
+#now  performing calculation
+sum=(num1+num2)
+print(sum)
+
+#division
+#give variable the numbers
+num3=(25)
+num4=(5)
+print(num3/num4)
+
+#Write a Python program to find the area of a triangle
+#enter the data
+base=10
+lenth=15
+#now by using formula finding area
+area=(1/2*base*lenth)
+print(area)
+
+
+#Write a Python program to convert Celsius to Fahrenheit
+#ceslsius is denoated by c
+c=37
+#convert celsius in fahrenheit
+F=(c*(9/5)+32)
+print(F)
+
